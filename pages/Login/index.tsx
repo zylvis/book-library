680251bf84cb6070ce543d5dbc7d7fd5374a92f5
Zilvinas Juodis <zilvinoj@gmail.com>
2022-09-26 20:40:29 +0300

    start

---
+++ pages/Login/index.tsx
@@ -0,0 +1,13 @@
+const Login = () => {
+    return(
+        <>
+            <form>
+                <label>User Name</label>
+                <input type="text"></input>
+                <label>Password</label>
+                <input type="text"></input>
+            </form>
+        </>
+    )
+}
+export default Login;
